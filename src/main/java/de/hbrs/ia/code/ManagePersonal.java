d3459f8fe9d203a1f8d90eb080e35855ee5c1297
Ömer Süzen <omsuz321@gmail.com>
2023-10-22 23:56:21 +0200

    some changes in the structure

---
+++ src/main/java/de/hbrs/ia/code/ManagePersonal.java
@@ -0,0 +1,21 @@
+package de.hbrs.ia.code;
+import java.util.List;
+
+import de.hbrs.ia.model.SalesMan;
+
+/**
+ * Code lines are commented for suppressing compile errors.
+ */
+public interface ManagePersonal {
+
+    public void createSalesMan( SalesMan record );
+
+    //public void addPerformanceReord( EvaluationRecord record , int sid );
+
+    public SalesMan readSalesMan( int sid );
+
+    public List<SalesMan> querySalesMan(String attribute , String key );
+
+    //public EvaluationRecord readEvaluationRecords( int sid );
+
+}
